93fdd6160cb5272f72d9e7295cd1a3eb1dbf329c
harshad Tingre <harshadstudy6@gmail.com>
2026-09-04 09:27:14 +0530

    Add files via upload

---
+++ area of circle.C
@@ -0,0 +1,22 @@
+/* area of circle is a = pi*r**
+Date:24 aug 2026 area=3.14*r*r
+--------------------------------------------------------------------------*/
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+
+
+	float area,r;
+	clrscr();
+	printf("\nEnter the radius of circle:");
+	scanf("%f",&r);
+	area = 3.14*r*r;
+	printf("\nThe Area of Circle is: %0.1f",area);
+
+
+
+
+
+	getch();
+}
