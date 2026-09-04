231c597bf398b92e42ad32af471f13c5f7a62e57
harshad Tingre <harshadstudy6@gmail.com>
2026-09-04 09:43:31 +0530

    area calculation in circle program
    Added a variable for pi and updated area calculation format.

--- area of circle.C
+++ area of circle.C
@@ -1,5 +1,5 @@
-/* area of circle is a = pi*r**
-Date:24 aug 2026 area=3.14*r*r
+/* area of circle is area = pi*r*r
+Date:24-aug-2026 , area=3.14*r*r
 --------------------------------------------------------------------------*/
 #include<stdio.h>
 #include<conio.h>
@@ -7,12 +7,13 @@ void main()
 {
 
 
-	float area,r;
+	float area,r,pi;
 	clrscr();
-	printf("\nEnter the radius of circle:");
+	printf("\nEnter the radius of circle: ");
 	scanf("%f",&r);
-	area = 3.14*r*r;
-	printf("\nThe Area of Circle is: %0.1f",area);
+	pi = 3.14;
+	area = pi*r*r;
+	printf("\nThe Area of Circle is: %0.2f",area);
 
 
 
