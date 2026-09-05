027176df70978aef581fcf5fd201c727d0bd4902
harshad Tingre <harshadstudy6@gmail.com>
2026-09-05 11:32:28 +0530

    Add files via upload

---
+++ WHILE_LO.C
@@ -0,0 +1,20 @@
+// Date 4-9-26,  topic== while loop
+#include<stdio.h>
+#include<conio.h>
+void main ()
+{
+	int i=1;
+	clrscr();
+	while (i<11){
+	printf("%d. hello world.\n",i);
+	i++;
+	}
+	printf("bye world");
+
+
+
+
+
+
+	getch();
+}
