58f83d6f5b53bec9eaf1e94c0f646653f09c557e
harshad Tingre <harshadstudy6@gmail.com>
2026-09-06 10:27:34 +0530

    Add files via upload

---
+++ area_rectangle.C
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main ()
+{
+	float length,height,area;
+	length=3.56f;
+	height=5.78f;
+	area=length*height;
+
+
+	clrscr();
+	printf("Area of Rectangle = %f",area);
+
+
+
+
+	getch();
+}
+
+
+
+
+
+
+
