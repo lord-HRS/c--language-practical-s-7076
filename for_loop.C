140721e13466d164602e3543850286135dff8f51
harshad Tingre <harshadstudy6@gmail.com>
2026-09-07 19:38:56 +0530

    Add files via upload

---
+++ for_loop.C
@@ -0,0 +1,17 @@
+// Date 4-9-26 , topic for loop
+#include<stdio.h>
+#include<conio.h>
+void main ()
+{
+	int i;
+	clrscr();
+	for(i=1; i<11; i++){
+	printf("%d.hlooo\n",i);
+	}
+	printf("helooooo");
+
+
+
+
+	getch();
+}
